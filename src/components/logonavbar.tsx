53d0022aa1c89cd4324428c3148b97e5b4721108
enggipratama <enggipratama82@gmail.com>
2025-04-21 04:42:26 +0800

    change font

--- src/components/logonavbar.tsx
+++ src/components/logonavbar.tsx
@@ -63,8 +63,11 @@ const FloatingLogoWithMenu: React.FC = () => {
             alt="Moon Logo"
             className="w-[clamp(3rem,3vw,3rem)] h-auto transition-transform duration-200 hover:scale-110"
           />
-          <h1 className="text-sm sm:text-l text-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-purple-700 to-pink-800 animate-pulse">
-            Double Tap_
+          <h1
+            style={{ fontFamily: "var(--font-varela), cursive" }}
+            className="text-xs text-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-purple-700 to-pink-800 animate-pulse"
+          >
+            Double Tap
           </h1>
         </div>
       </Draggable>
@@ -79,7 +82,7 @@ const FloatingLogoWithMenu: React.FC = () => {
             className="fixed text-center z-40 bg-blue-900 rounded-lg shadow-lg px-4 py-3 max-w-[90vw]"
             style={{
               top: logoPosition.y + menuOffset + 10,
-              left: logoPosition.x - menuOffset + 20,
+              left: logoPosition.x - menuOffset + 30,
               transform: "translateX(-50%)",
               width: "clamp(8rem, 10vw, 10rem)",
             }}
@@ -92,7 +95,8 @@ const FloatingLogoWithMenu: React.FC = () => {
                     e.preventDefault();
                     handleScrollTo("home");
                   }}
-                  className="block py-2 text-white text-sm sm:text-base transition-transform duration-200 transform hover:scale-120"
+                  style={{ fontFamily: "var(--font-varela), cursive" }}
+                  className="block py-2 text-white text-sm transition-transform duration-200 transform hover:scale-120"
                 >
                   Home
                 </a>
@@ -104,7 +108,8 @@ const FloatingLogoWithMenu: React.FC = () => {
                     e.preventDefault();
                     handleScrollTo("about");
                   }}
-                  className="block py-2 text-white text-sm sm:text-base transition-transform duration-200 transform hover:scale-120"
+                  style={{ fontFamily: "var(--font-varela), cursive" }}
+                  className="block py-2 text-white text-sm transition-transform duration-200 transform hover:scale-120"
                 >
                   About Me
                 </a>
@@ -116,7 +121,8 @@ const FloatingLogoWithMenu: React.FC = () => {
                     e.preventDefault();
                     handleScrollTo("portofolio");
                   }}
-                  className="block py-2 text-white text-sm sm:text-base transition-transform duration-200 transform hover:scale-120"
+                  style={{ fontFamily: "var(--font-varela), cursive" }}
+                  className="block py-2 text-white text-sm transition-transform duration-200 transform hover:scale-120"
                 >
                   Portofolio
                 </a>
@@ -128,7 +134,8 @@ const FloatingLogoWithMenu: React.FC = () => {
                     e.preventDefault();
                     handleScrollTo("contact");
                   }}
-                  className="block py-2 text-white text-sm sm:text-base transition-transform duration-200 transform hover:scale-120"
+                  style={{ fontFamily: "var(--font-varela), cursive" }}
+                  className="block py-2 text-white text-sm transition-transform duration-200 transform hover:scale-120"
                 >
                   Contact
                 </a>
